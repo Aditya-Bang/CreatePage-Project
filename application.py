145abc109322e768a74a1c460b3bbd97cbf56425
Aditya Bang <adityabang15@gmail.com>
2024-05-15 18:31:42 -0400

    readme change

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+import sqlalchemy
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -30,8 +31,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///createpage.db")
+# Configure DB
+# db = SQL("sqlite:///createpage.db")
+db = SQL(os.environ["DATABASE_URL"])
 
 # colour options
 colors = [{"color": "Black", "code": "#000000"}, {"color": "White", "code": "#FFFFFF"}, {"color": "Red", "code": "#FF0000"},
@@ -525,3 +527,4 @@ def errorhandler(e):
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
